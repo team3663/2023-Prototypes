12951c90e1b431d3f045b43d4255e30afa9badd4
Richard Elmore <richardelmore@users.noreply.github.com>
2023-02-11 12:56:32 -0800

    Update robot container and contstants.

--- Prototypes/src/main/java/frc/robot/Constants.java
+++ Prototypes/src/main/java/frc/robot/Constants.java
@@ -1,25 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int DRIVE_CONTROLLER_PORT = 0;
-    public static final int OPERATOR_CONTROLLER_PORT = 1;
-    public static final int TEST_CONTROLLER_PORT = 2;
+  public static class ControllerPorts {
+    public static final int DRIVER = 0;
+    public static final int OPERATOR= 1;
+    public static final int TEST = 2;
   }
 
   public final class CanIds {
-    public static final int MOTOR1_ID = 0; 
+    public static final int SHOULDER_MOTOR = 5;
+    public static final int ELBOW_MOTOR = 6;
+    public static final int WRIST_MOTOR = 7; 
+    public static final int INTAKE_MOTOR = 8; 
   }
 }
--- Prototypes/src/main/java/frc/robot/RobotContainer.java
+++ Prototypes/src/main/java/frc/robot/RobotContainer.java
@@ -4,24 +4,19 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
+import frc.robot.Constants.ControllerPorts;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 
 /**
  * This class is where the bulk of the robot should be declared.
  */
 public class RobotContainer {
 
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final CommandXboxController m_driverController = new CommandXboxController(
-      OperatorConstants.DRIVE_CONTROLLER_PORT);
+  private final CommandXboxController driverController = new CommandXboxController(ControllerPorts.DRIVER);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -44,14 +39,7 @@ public class RobotContainer {
    * Use this method to define your trigger->command mappings.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-    // pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -61,6 +49,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
