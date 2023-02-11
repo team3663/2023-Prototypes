34caef2f5d89b5c0737c08910ae9ac755013f699
joshuarus <97773720+joshuarus@users.noreply.github.com>
2023-02-11 13:12:27 -0800

    Create initial files

--- Prototypes/src/main/java/frc/robot/commands/Autos.java
+++ Prototypes/src/main/java/frc/robot/commands/Autos.java
@@ -4,13 +4,13 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
+  public static CommandBase exampleAuto(ArmSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
 
