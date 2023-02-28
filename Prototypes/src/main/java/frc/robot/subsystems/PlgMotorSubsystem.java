a9ec793ead0977445421070be21b136b54e8d5ed
Richard Elmore <richardelmore@live.com>
2023-02-27 16:04:06 -0800

    Final comment cleanup to finish example.

--- Prototypes/src/main/java/frc/robot/subsystems/PlgMotorSubsystem.java
+++ Prototypes/src/main/java/frc/robot/subsystems/PlgMotorSubsystem.java
@@ -7,6 +7,13 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
+/**
+ * Example of subsystem to control a Johnson Electric JE-PLG-149 brushed DC motor and read it's encoder.
+ * using a SparkMAX motor controller.
+ * 
+ * We used information from this data sheet to write the code and wire the motor to the Spark's encoder port.
+ * https://first.wpi.edu/FRC/roborio/Docs/2020-02-12_JE-PLG-149_R2.pdf
+ */
 public class PlgMotorSubsystem extends SubsystemBase {
 
   private CANSparkMax spark;
@@ -16,32 +23,27 @@ public class PlgMotorSubsystem extends SubsystemBase {
     spark = new CANSparkMax(1, MotorType.kBrushed);
     spark.restoreFactoryDefaults();
 
-    // This countsPerRev value (176) seems odd, based on expirements we are guessing that it means
-    // the number of edges (both rising and falling) per revolution for BOTH quadrature
-    // channels combined.
-    // So the specifed 44 pulses per revolution (both A & B combined) times 4 (4 edges per cycle).
-    // This seems weird but it results the correct velocity from from getVelocity()
-    encoder = spark.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 176);
+    // The PLG encoder emits 44.4 pulses per revolution of the output shaft.  The two outputs
+    // are 45 degrees out of phase so each pulse contains 4 quadrature state transitions.
+    // That works out to 44.4 * 4 = 177.6 state transitions per revolution.
+    // This is the value REVLib wants for the countsPerRev parameter to getEncoder().
+    // Since countsPerRev is an int we round up to 178.
+    encoder = spark.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 178);
     encoder.setInverted(true);
     encoder.setAverageDepth(64);
   }
 
   @Override
   public void periodic() { 
+
     Logger.getInstance().recordOutput("Motor/Velocity", encoder.getVelocity());
     Logger.getInstance().recordOutput("Motor/Position", encoder.getPosition());
-    Logger.getInstance().recordOutput("Motor/CountsPerRev", encoder.getCountsPerRevolution());
-    Logger.getInstance().recordOutput("Motor/VelocityConversionFactor", encoder.getVelocityConversionFactor());
   }
 
-  public void setPower(Double power){
+  public void setPower(Double power) {
     spark.set(power);
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
   public void resetEncoder(){
     encoder.setPosition(0);
   }
