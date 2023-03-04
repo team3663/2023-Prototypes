e85f8fc47b670ae0c93b85c1481fa5e2450c051c
Kaitlin Moody <97428270+Spicykait@users.noreply.github.com>
2023-03-04 10:12:39 -0800

    motors

--- Prototypes/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ Prototypes/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -3,15 +3,38 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAnalogSensor.Mode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
 public class DriveTrainSubsystem extends SubsystemBase {
   /** Creates a new DriveTrainSubsystem. */
-  public DriveTrainSubsystem() {}
+  private final CANSparkMax motor;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	public DrivetrainSubsystem(int SPARKmotorIdRIGHT, int SPARKmotorIdLEFT) {
+    motor = new CANSparkMax(SPARKmotorIdRIGHT, MotorType.kBrushed);
+		motor.setInverted(true);
+		motor.restoreFactoryDefaults();
+
+    motor = new CANSparkMax(SPARKmotorIdLEFT, MotorType.kBrushed);
+		motor.setInverted(true);
+		motor.restoreFactoryDefaults();
+	}
+
+	@Override
+	public void periodic() {
+		System.out.println(motor.getOutputCurrent());
+	}
+
+	public void setPower(double power) {
+		motor.set(power);
+		System.out.println("===== output current: " +motor.getOutputCurrent());
+	}
 }
+
+
+  
